10687cb97a60277307543e3558ac01061b247603
CapsAdmin <eliashogstvedt@gmail.com>
2010-02-21 12:18:48 +0000

    Added CowThing's aquamine.

---
+++ lua/entities/fishing_mod_aquamine/shared.lua
@@ -0,0 +1,3 @@
+ENT.Type = "anim"
+ENT.Base = "fishing_mod_base"
+ENT.PrintName = "Aquamine"
---
+++ lua/fishing_mod/catch/aquamine.lua
@@ -0,0 +1,23 @@
+fishingmod.AddCatch{
+	friendly = "Aquamine",
+	type = "fishing_mod_aquamine",
+	rareness = 2000, 
+	yank = 0, 
+	force = 0, 
+	mindepth = 200, 
+	maxdepth = 20000,
+	expgain = 30,
+	levelrequired = 3,
+	remove_on_release = false,
+	friendlybait = "Melons",
+	bait = {
+		"models/props_junk/watermelon01.mdl",
+		"models/props_junk/watermelon01_chunk01a.mdl",
+		"models/props_junk/watermelon01_chunk01b.mdl",
+		"models/props_junk/watermelon01_chunk01c.mdl",
+		"models/props_junk/watermelon01_chunk02a.mdl",
+		"models/props_junk/watermelon01_chunk02b.mdl",
+		"models/props_junk/watermelon01_chunk02b.mdl",
+		"models/props_junk/watermelon01_chunk02c.mdl",
+	},
+}
