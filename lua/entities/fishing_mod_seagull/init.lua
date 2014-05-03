d450433378d20f47d07f7ec74806682304419d6c
Noiwex <noiwex1911@gmail.com>
2014-05-03 20:46:28 +0400

    Revert "Replaced player.GetByUniqueID with :GetOwner() in hope that it'll fix performance issues ( Curse at you, Garry! :( )"
    This reverts commit 7e5e90e052251109ea39385461b27ad069d856f0.

--- lua/entities/fishing_mod_seagull/init.lua
+++ lua/entities/fishing_mod_seagull/init.lua
@@ -31,7 +31,7 @@ function ENT:PickTarget()
 	for key, catch in RandomPairs(ents.GetAll()) do
 		local owner 
 		if catch.data then
-			owner = catch:GetOwner()
+			owner = player.GetByUniqueID(catch.data.ownerid)
 		end
 		if 
 			catch:GetNWBool("fishingmod catch") and 
