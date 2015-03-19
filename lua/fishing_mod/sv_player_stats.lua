543f8f3bf98d4344b4c8ed6a7e8f413e1aefb0f1
PotcFdk <PotcFdk@gmx.net>
2015-03-19 19:23:27 +0100

    Created a binary storage format for fishingmod stats. Old data will be migrated automatically the next time it's used.

--- lua/fishing_mod/sv_player_stats.lua
+++ lua/fishing_mod/sv_player_stats.lua
@@ -1,11 +1,150 @@
-function fishingmod.SavePlayerInfo(ply, name, data)
-	file.CreateDir("fishingmod")
-	file.CreateDir("fishingmod/"..ply:UniqueID())
-	file.Write("fishingmod/"..ply:UniqueID().."/"..name..".txt", data)
-end
+--[[ Format:
+	0x00  : VERSION (currently: 0x01)
+	0x00+1: catches
+	0x08+1: exp
+	0x10+1: money
+	0x18+1: length
+	0x20+1: reel_speed
+	0x28+1: string_length
+	0x30+1: force
+]]
+
+local VERSION = 0x01
+
+local POS_VERSION    = 0x01
+local POS_CATCHES    = 0x00 + 1
+local POS_EXP        = 0x08 + 1
+local POS_MONEY      = 0x10 + 1
+local POS_LENGTH     = 0x18 + 1
+local POS_REEL_SPEED = 0x20 + 1
+local POS_STRING_LEN = 0x28 + 1
+local POS_FORCE      = 0x30 + 1
+
+local POSITIONS = {
+	catches       = POS_CATCHES,
+	exp           = POS_EXP,
+	money         = POS_MONEY,
+	length        = POS_LENGTH,
+	reel_speed    = POS_REEL_SPEED,
+	string_length = POS_STRING_LEN,
+	force         = POS_FORCE
+}
+
+-- MIGRATION
+	local MIGRATION = {}
+	
+	local function MIGRATION_SAVE_NEW_DATA (ply, data)
+		local uid = ply:UniqueID()
+		file.CreateDir("fishingmod")
+		file.CreateDir("fishingmod/"..uid:sub(1,1))
+		
+		local fh = file.Open("fishingmod/"..uid:sub(1,1).."/"..uid..".txt", "wb", "DATA")
+		assert (fh, "Error opening file for player "..tostring(ply))
+		fh:WriteByte(VERSION)
+		
+		fh:WriteDouble(data.catches       or 0)
+		fh:WriteDouble(data.exp           or 0)
+		fh:WriteDouble(data.money         or 0)
+		fh:WriteDouble(data.length        or 0)
+		fh:WriteDouble(data.reel_speed    or 0)
+		fh:WriteDouble(data.string_length or 0)
+		fh:WriteDouble(data.force         or 0)
+		fh:Close()
+	end
+	
+	-- old fishingmod
+		table.insert (MIGRATION, {
+			check = function (ply)
+				return file.IsDir("fishingmod/"..ply:UniqueID(), "DATA")
+			end,
+			read = function (ply)
+				local data = {}
+				for _, name in next, (file.Find("fishingmod/"..ply:UniqueID().."/*.txt", "DATA")) do
+					data [name:sub(1,-5)] = tonumber(file.Read("fishingmod/"..ply:UniqueID().."/"..name, "DATA")) or 0
+				end
+				return data
+			end,
+			cleanup = function (ply)
+				for _, name in next, (file.Find("fishingmod/"..ply:UniqueID().."/*.txt", "DATA")) do
+					file.Delete("fishingmod/"..ply:UniqueID().."/"..name)
+				end
+				file.Delete("fishingmod/"..ply:UniqueID())
+			end
+		})
+	-- / old fishingmod
+-- / MIGRATION
 
 function fishingmod.LoadPlayerInfo(ply, name)
-	return file.Read("fishingmod/"..ply:UniqueID().."/"..name..".txt")
+	for _, M in next, MIGRATION do
+		if M.check (ply) then
+			Msg ("[fishingmod] ") print ("Can migrate old fishingmod data from player: "..tostring(ply).."...")
+			local data = M.read (ply)
+			PrintTable (data)
+			MIGRATION_SAVE_NEW_DATA (ply, data)
+			M.cleanup (ply)
+			print ("Success.")
+		end
+	end
+	
+	if name then -- read single info
+		assert(POSITIONS[name], "Unknown data name '"..tostring(name).."'")
+		local uid = ply:UniqueID()
+		local filep = "fishingmod/"..uid:sub(1,1).."/"..uid..".txt"
+		if not file.Exists(filep, "DATA") then return end
+		local fh = file.Open(filep, "rb", "DATA")
+		assert (fh, "Error opening file for player "..tostring(ply))
+		local version = fh:ReadByte()
+		if version ~= VERSION then
+			fh:Close() error("Unsupported version: " .. version)
+		end
+		fh:Seek(POSITIONS[name])
+		local data = fh:ReadDouble()
+		fh:Close()
+		return data
+	else -- read all info
+		local uid = ply:UniqueID()
+		local filep = "fishingmod/"..uid:sub(1,1).."/"..uid..".txt"
+		if not file.Exists(filep, "DATA") then return end
+		local fh = file.Open(filep, "rb", "DATA")
+		assert(fh, "Error opening file for player "..tostring(ply))
+		local version = fh:ReadByte()
+		if version ~= VERSION then
+			fh:Close() error("Unsupported version: " .. version)
+		end
+		
+		local data = {}
+		
+		for info_n, info_p in next, POSITIONS do
+			fh:Seek(info_p)
+			data [info_n] = fh:ReadDouble()
+		end
+		
+		fh:Close()
+		return data
+	end
+end
+
+function fishingmod.SavePlayerInfo(ply, name, data)
+	assert(POSITIONS[name], "Unknown data name '"..tostring(name).."'")
+	local uid = ply:UniqueID()
+	file.CreateDir("fishingmod")
+	file.CreateDir("fishingmod/"..uid:sub(1,1))
+	
+	local p_data = fishingmod.LoadPlayerInfo(ply) or {}
+	p_data [name] = data
+	
+	local fh = file.Open("fishingmod/"..uid:sub(1,1).."/"..uid..".txt", "wb", "DATA")
+	assert (fh, "Error opening file for player "..tostring(ply))
+	fh:WriteByte(VERSION)
+	
+	fh:WriteDouble(p_data.catches       or 0)
+	fh:WriteDouble(p_data.exp           or 0)
+	fh:WriteDouble(p_data.money         or 0)
+	fh:WriteDouble(p_data.length        or 0)
+	fh:WriteDouble(p_data.reel_speed    or 0)
+	fh:WriteDouble(p_data.string_length or 0)
+	fh:WriteDouble(p_data.force         or 0)
+	fh:Close()
 end
 
 function fishingmod.GainEXP(ply, amount)
