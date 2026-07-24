046fad7b672f6815013b8550df2c8885958fb429
Ryan <20880060+Earu@users.noreply.github.com>
2026-07-24 02:07:59 +0200

    this also doesnt need its whole category

--- lua/weapons/weapon_fishing_rod.lua
+++ lua/weapons/weapon_fishing_rod.lua
@@ -1,5 +1,4 @@
 SWEP.Author = "CapsAdmin"
-SWEP.Category = "Fishing Mod"
 SWEP.Instructions = "To reel down, hold left mouse button\nTo reel up, hold right mouse button\nTo reel faster, hold shift\nTo reel slower, hold alt\nTo release bait, press e\nTo release catch, press r\nTo access the menu, press B"
 SWEP.Spawnable = true 
 SWEP.ViewModel = Model("models/weapons/v_hands.mdl")
